c5b6bef9124cf1e84d235f039b803f0829c9824e
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-04-25 16:19:47 +0100

    add startTimer() and stopTimer()

---
+++ src/js/timer.js
@@ -0,0 +1,37 @@
+//get the html element thath show the timer
+
+// set seconds and minutes variables
+let seconds = 0;
+let minutes = 0;
+//set timer variable
+let timer;
+
+const startTimer = () => {
+  timer = setInterval(() => {
+    if (minutes === 0) {
+      //   timerSpan.textContent = `${seconds} seconds`;
+      console.log(seconds);
+      seconds++;
+      if (seconds > 5) {
+        minutes++;
+        seconds = 1;
+      }
+    } else {
+      //   timerSpan.textContent = `${minutes} minutes ${seconds} seconds`;
+      console.log(minutes, seconds);
+      seconds++;
+      if (seconds > 5) {
+        minutes++;
+        seconds = 1;
+      }
+    }
+  }, 1000);
+};
+
+const stopTimer = () => {
+  clearInterval(timer);
+  seconds = 0;
+  minutes = 0;
+};
+
+export { startTimer, stopTimer };
