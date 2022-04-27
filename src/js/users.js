82a8e4020cc8f989667c4783d51dd9ab7c2feffe
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-04-27 14:41:33 +0100

    modify addUser(), add function to set currentUser to localStorage

--- src/js/users.js
+++ src/js/users.js
@@ -1,3 +1,5 @@
+import { getLocalStorage } from "./score-board.js";
+
 const user_input = document.getElementById("user_input");
 
 const startGameButton = document.getElementById("start_game");
@@ -5,8 +7,17 @@ const startGameButton = document.getElementById("start_game");
 let userList = [];
 
 const addUsers = () => {
-  userList.push({ name: user_input.value, score: 0 });
+  userList.push({ name: user_input.value, score: 0, currentPlaying: "" });
   user_input.value = "";
 };
 
-export { addUsers, userList };
+const addUsersToLocalStorage = () => {
+  localStorage.setItem("users", JSON.stringify(userList));
+};
+
+const nextPlayer = () => {
+  let users = getLocalStorage();
+  users[0].currentPlaying = "Current Playing";
+};
+
+export { addUsers, userList, addUsersToLocalStorage };
